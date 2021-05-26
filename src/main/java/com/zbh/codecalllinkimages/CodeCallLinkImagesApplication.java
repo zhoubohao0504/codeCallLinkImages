860dde6dae6786d6204446f70ebc763e0742becc
zhoubohao0504 <734800073@qq.com>
2021-05-26 15:58:55 +0800

    first commit

---
+++ src/main/java/com/zbh/codecalllinkimages/CodeCallLinkImagesApplication.java
@@ -0,0 +1,13 @@
+package com.zbh.codecalllinkimages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCallLinkImagesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeCallLinkImagesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zbh/codecalllinkimages/CodeCallLinkImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbh.codecalllinkimages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCallLinkImagesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
